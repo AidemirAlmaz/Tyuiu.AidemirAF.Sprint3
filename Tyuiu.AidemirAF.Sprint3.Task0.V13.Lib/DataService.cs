65785ff26c91c8cfcf576c7db47ce884b0a55433
Tyuiu.AidemirAF@outlook.com <almazaydemir@yandex.ru>
2024-10-16 23:45:55 +0500

    Add project files.

---
+++ Tyuiu.AidemirAF.Sprint3.Task0.V13.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.AidemirAF.Sprint3.Task0.V13.Lib
+{
+    public class DataService : ISprint3Task0V13
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double a = 0.5;
+            double sumSeries = 0;
+            int i;
+            for (i = startValue; i <= stopValue; i++)
+            {
+                sumSeries = sumSeries + (Math.Pow(a, i) + (1 / 2)) * Math.Cos(i); 
+            }
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.AidemirAF.Sprint3.Task0.V13/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.AidemirAF.Sprint3.Task0.V13.Lib;
+
+/*
+ * ЗАДАНИЕ
+ * Написать программу используя цикл for,
+ * которая вычисляет сумму ряда по формуле, при а = 0,5.
+ */
+
+namespace Tyuiu.AidemirAF.Sprint3.Task0.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("*****************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                  *");
+            Console.WriteLine("*****************************************************************************************************");
+
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 20;
+            Console.WriteLine("Переменная a = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+
+            Console.WriteLine("*****************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                        *");
+            Console.WriteLine("*****************************************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
