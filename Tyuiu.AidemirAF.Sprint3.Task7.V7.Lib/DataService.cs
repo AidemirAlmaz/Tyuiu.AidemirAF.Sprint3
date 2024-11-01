737f0027555a37517f9cb2e86df239cdf0b44cf3
Tyuiu.AidemirAF@outlook.com <almazaydemir@yandex.ru>
2024-11-02 01:51:07 +0500

    Task7

---
+++ Tyuiu.AidemirAF.Sprint3.Task7.V7.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.AidemirAF.Sprint3.Task7.V7.Lib
+{
+    public class DataService : ISprint3Task7V7
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray; //объявляем пустой массив
+            int len = (stopValue - startValue) + 1; //вычисляем длину массива
+            valueArray = new double[len]; //создаем массив с указанной длиной
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x != -2)
+                {
+                    y = Math.Round(Math.Cos(x) + (Math.Cos(x) / (x + 2)) - 3 * x, 2); //округляем значение до 2 знаков после запятой
+                    valueArray[count] = y; //присваиваем значение в массив с индексом count
+                    count++;
+                }
+                else
+                {
+                    y = 0;
+                }
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.AidemirAF.Sprint3.Task7.V7.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+using Tyuiu.AidemirAF.Sprint3.Task7.V7.Lib;
+
+namespace Tyuiu.AidemirAF.Sprint3.Task7.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = 0;
+            int stopValue = 1;
+            int len = stopValue - startValue + 1;
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+            valueWaitArray[0] = 1.5;
+            valueWaitArray[1] = -2.28;
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(valueWaitArray, res);
+        }
+    }
+}
